627d4f6877fc5cebe5363a0ec800dc43adb7610f
cpchrispye <cpchrispye@hotmail.com>
2017-09-21 17:44:53 +0100

    simplefied  descripor handlers
    also added handler ability to list type

--- PyDynamicStructures/dynamic_structure.py
+++ PyDynamicStructures/dynamic_structure.py
@@ -90,6 +90,10 @@ class LittleEndian(BaseType):
 
 class BYTE(BigEndian):
     BASEFORMAT = 'c'
+    DEFAULTVALUE = '\0'
+
+    def __repr__(self):
+        return self.__class__.__name__ + ": 0x" +  self.internal_value.encode("hex")
 
 
 class UINT8(BigEndian):
@@ -138,6 +142,7 @@ class STRING(BigEndian):
 
 class BYTE_L(LittleEndian):
     BASEFORMAT = 'c'
+    DEFAULTVALUE = '\0'
 
 
 class UINT8_L(LittleEndian):
@@ -198,9 +203,10 @@ class DynObject(object):
         :param key:
         :return: value
         """
+        getter = '__get__'
         v = object.__getattribute__(self, key)
-        if hasattr(v, '__get__'):
-            return v.__get__(self, type(self))
+        if hasattr(v, getter):
+            return getattr(v, getter)(self, type(self))
         return v
 
     def __setattr__(self, key, value):
@@ -210,62 +216,64 @@ class DynObject(object):
         :param value:
         :return:
         """
-        if key not in self.__dict__:
-            # if this is the first time set attribute to the what ever is passed
-            object.__setattr__(self, key, value)
-        else:
-            # if attribute has __set__ we call its __set__ method unless the value we are setting has a __set__ method
-            # then its likely that the user is trying to replace the descriptor. The user may wish to replace the
-            # descriptor with another obect thats not a descriptor in which case the object should have a REPLACE attribute set to True
+        setter = '__set__'
+        try:
             v = self.__dict__[key]
-            if (hasattr(v, '__set__')
-                    and not (hasattr(value, '__set__')
-                             or (hasattr(value, 'REPLACE') and value.REPLACE==True))):
-                # set the property
-                v.__set__(self, value)
-            else:
-                # set the attribute
-                object.__setattr__(self, key, value)
+        except KeyError:
+            object.__setattr__(self, key, value)
+            return None
 
-    def get_attr_list_with(self, attr='__get__'):
-        """
-        gets a list of attributes with attribute
-        :return: :class:`dict`
-        """
-        descriptors = {}
-        for key, item in self.__dict__.items():
-            if hasattr(item, attr):
-                descriptors[key] = (item)
-        return descriptors
+        # if attribute has __set__ we call its __set__ method unless the value we are setting has a __set__ method
+        # then its likely that the user is trying to replace the descriptor. The user may wish to replace the
+        # descriptor with another obect thats not a descriptor in which case the object should have a REPLACE attribute set to True
+        if (hasattr(v, setter)
+            and not (hasattr(value, setter)
+                     or (hasattr(value, 'REPLACE') and value.REPLACE == True))):
+            # set the property
+            setattr(v, setter, value)
+        else:
+            # set the attribute
+            object.__setattr__(self, key, value)
+
 
 class OrderedDynObject(DynObject):
 
     def __setattr__(self, key, value):
-        if len(self.__dict__) == 0:
-            self.__dict__['_fields'] = OrderedDict()
-        if key not in self.__dict__['_fields']:
-            self._fields[key] = None
+        try:
+            fields = self.__dict__['_fields']
+        except KeyError:
+            fields = OrderedDict()
+            self.__dict__['_fields'] = fields
+
+        if key not in fields:
+            fields[key] = None
         super(OrderedDynObject, self).__setattr__(key, value)
 
     def __getattr__(self, item):
-        if len(self.__dict__) == 0:
-            self.__dict__['_fields'] = OrderedDict()
+        if item == '_fields':
+            fields = OrderedDict()
+            self.__dict__['_fields'] = fields
+            return fields
         return super(OrderedDynObject, self).__getattribute__(item)
-
-    def get_attr_list_with(self, attr='__get__'):
-        """
-        gets a list of attributes with attribute
-        :return: :class:`dict`
-        """
-        descriptors = OrderedDict()
-        for key in self.attributes():
-            if key in self.__dict__ and hasattr(self.__dict__[key], attr):
-                descriptors[key] = self.__dict__[key]
-        return descriptors
 
     def attributes(self):
         return self._fields.keys()
 
+
+def get_dict_attr(obj, attr):
+    for obj in [obj] + obj.__class__.mro():
+        if attr in obj.__dict__:
+            return obj.__dict__[attr]
+    raise AttributeError
+
+
+def get_attributes_with(ordered_dyn_object, has_attribute):
+    out = OrderedDict()
+    for attr_name in ordered_dyn_object.attributes():
+        attr = get_dict_attr(ordered_dyn_object, attr_name)
+        if hasattr(attr, has_attribute):
+            out[attr_name] = attr
+    return out
 
 class Structure(OrderedDynObject):
     """
@@ -285,7 +293,7 @@ class Structure(OrderedDynObject):
 
     def pack(self):
         byte_data = bytes()
-        for struct in self.get_attr_list_with('pack').values():
+        for struct in get_attributes_with(self, 'pack').values():
             byte_data += struct.pack()
         return byte_data
 
@@ -293,47 +301,51 @@ class Structure(OrderedDynObject):
         index = 0
         self.__offset = offset
         self.__buffer = buffer
-        for struct in self.get_attr_list_with('unpack').values():
+        for struct in get_attributes_with(self, 'unpack').values():
             index += struct.unpack(buffer, index + offset)
         return index
 
     def update(self):
         index = 0
-        for struct in self.get_attr_list_with('unpack').values():
+        for struct in get_attributes_with(self, 'unpack').values():
             index += struct.unpack(self.__buffer, index + self.__offset)
 
     def size(self):
         size_in_bytes = 0
-        for struct in self.get_attr_list_with('size').values():
+        for struct in get_attributes_with(self, 'size').values():
             size_in_bytes += struct.size()
         return size_in_bytes
 
     def set_values(self, values):
         if isinstance(values, dict):
             for key, val in values.items():
-                if hasattr(self.__getattribute__(key), 'set_values'):
-                    self.__getattribute__(key).set_values(val)
+                attr = getattr(self, key)
+                if hasattr(attr, 'set_values'):
+                    attr.set_values(val)
                 else:
-                    self.__setattr__(key, val)
+                    setattr(self, key, val)
+
         elif isinstance(values, Sequence):
             index = 0
             for key in self.attributes():
                 if index >= len(values):
                     break
-                if hasattr(self.__getattribute__(key), 'set_values'):
-                    index += self.__getattribute__(key).set_values(values[index:])
+                attr = getattr(self, key)
+                if hasattr(attr, 'set_values'):
+                    index += attr.set_values(values[index:])
                 else:
-                    self.__setattr__(key, values[index])
+                    setattr(self, key, values[index])
                     index += 1
             return index
 
     def add_field(self, name, type_val, length=None):
         if isinstance(type(type_val), type):
             type_val = type_val()
+
         if length is None:
-            self.__setattr__(name, type_val)
+            setattr(self, name, type_val)
         elif length is not None:
-            self.__setattr__(name, type_val * length)
+            setattr(self, name, type_val * length)
 
     def add_fields(self, fields):
         for field in fields:
@@ -346,14 +358,6 @@ class Structure(OrderedDynObject):
                 raise Exception("_fields_ takes 2 or 3 colomns")
             self.add_field(name, type, length)
 
-    def get(self, name=None):
-        out = OrderedDict()
-        for n in self.attributes():
-            out[n] = self.__dict__[n]
-        if name is not None:
-            return out[name]
-        return out
-
     @classmethod
     def __mul__(cls, other):
         return StructureList([cls() for _ in range(int(other))])
@@ -365,6 +369,43 @@ class Structure(OrderedDynObject):
 
 class StructureList(list):
     REPLACE = True
+
+
+    def __getitem__(self, key):
+        """
+        :param key:
+        :return: value
+        """
+        getter = '__get__'
+        v = list.__getitem__(self, key)
+        if hasattr(v, getter):
+            return getattr(v, getter)(self, type(self))
+        return v
+
+    def __setitem__(self, key, value):
+        """
+
+        :param key:
+        :param value:
+        :return:
+        """
+        setter = '__set__'
+        try:
+            v = list.__getitem__(self, key)
+        except KeyError:
+            return super(list, self).__setitem__(key, value)
+
+        # if attribute has __set__ we call its __set__ method unless the value we are setting has a __set__ method
+        # then its likely that the user is trying to replace the descriptor. The user may wish to replace the
+        # descriptor with another obect thats not a descriptor in which case the object should have a REPLACE attribute set to True
+        if (hasattr(v, setter)
+            and not (hasattr(value, setter)
+                     or (hasattr(value, 'REPLACE') and value.REPLACE == True))):
+            # set the property
+            setattr(v, setter, value)
+        else:
+            # set the attribute
+            return super(list, self).__setitem__(key, value)
 
     def pack(self):
         byte_data = bytes()
@@ -426,7 +467,8 @@ class Selector(object):
         pass
 
     def pack(self):
-        self.internal_state = self.select(**self.kwargs)
+        if self.internal_state is None:
+            raise Exception("Selector needs to be initialized call unpack() on it")
         return self.internal_state.pack()
 
     def unpack(self, buffer, offset=0):
@@ -477,6 +519,8 @@ if __name__ == "__main__":
 
     hd.unpack(header_data)
     d = hd.pack()
+    print(d.encode('hex'))
+    print(data)
     alt_struct = sub_alt(3, 5)
     hd.options = alt_struct
     hd.update()
